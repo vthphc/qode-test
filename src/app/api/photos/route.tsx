b064f1d8b674747c58876dee767157214a1c48c4
tkj3nfuq <thinphcv5@gmail.com>
2024-10-11 19:43:48 +0700

    Update eslint configuration and remove unused imports

--- src/app/api/photos/route.tsx
+++ src/app/api/photos/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient, Photo } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
